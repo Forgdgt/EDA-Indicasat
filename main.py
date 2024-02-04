77600aae56f13de96fef753992ee13e14f866304
Fordgt <ricordpty@gmail.com>
2024-02-04 14:12:07 -0500

    title upload file

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
 import streamlit as st 
 import pandas as pd
 import numpy as np
-import plotly.express as px
 
+# Web App Title
+st.markdown('''
+# **The EDA App**
+
+This is the **EDA App** created in Streamlit using the **pandas-profiling** library.
+
+**Credit:** App built in `Python` + `Streamlit` by [Chanin Nantasenamat](https://medium.com/@chanin.nantasenamat) (aka [Data Professor](http://youtube.com/dataprofessor))
+
+---
+''')
+
+# Upload CSV data
+with st.sidebar.header('1. Upload your CSV data'):
+    uploaded_file = st.sidebar.file_uploader("Upload your input CSV file", type=["csv"])
+    st.sidebar.markdown("""
+[Example CSV input file](https://raw.githubusercontent.com/dataprofessor/data/master/delaney_solubility_with_descriptors.csv)
+""")
